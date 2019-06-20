53e2320a801ce52e33fe8feb298285a792be9d72
Jherom <jherom.chacon@gmail.com>
2019-06-20 10:34:14 -0600

    Agregando clase Login

---
+++ FacturacionSoda/src/Seguridad/Login.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Seguridad;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jheron Chacon
+ */
+public class Login {
+    public Date fechaLogueo;
+    public Usuario usuarioLogueo;
+}
