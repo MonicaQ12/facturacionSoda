19681c15b5609fb752cccff165a95567e0c427cf
Jherom <jherom.chacon@gmail.com>
2019-06-20 10:04:15 -0600

    Versión preliminar del profesor del sistema de facturación soda.

---
+++ FacturacionSoda/src/facturacionsoda/FacturacionSoda.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturacionsoda;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jheron Chacon
+ */
+public class FacturacionSoda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu miMenu = new Menu();
+        List<Producto> productos = miMenu.getProductos();
+        for(int i = 0; i < productos.size(); i++){
+            System.out.println(productos.get(i).getNombre());
+        }
+    }
+    
+}
