b7b3a1760fd1c5018ee6b6c0ace3764d60c645e6
Jherom <jherom.chacon@gmail.com>
2019-07-02 09:31:08 -0600

    Se agregan las clases Ensalada y Sandwiche implementando herencia desde la clase ancestral producto.
    Se refactoriza la clase producto para ordenar la estructura de la clase

---
+++ FacturacionSoda/src/facturacionsoda/Sandwiche.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturacionsoda;
+
+/**
+ *
+ * @author Jheron Chacon
+ */
+public class Sandwiche extends Producto{
+    
+    public final static String CIABATA = "Ciabata";
+    public final static String INTEGRAL = "Integral";
+    public final static String AJONJOLI = "Ajonjoli";
+    public final static String BLANCO = "Blanco";
+    public final static String CUADRADO_BLANCO = "Cuadrado blanco";
+    
+    public String tipoPan;
+    
+    public Sandwiche(){}
+    
+    public Sandwiche(String nombre){}
+}
